4cfc5fb1e8901cf5e23132f7acc7faa57eb89e55
waqarcs11 <adsknock@gmail.com>
2025-08-28 18:41:11 +0500

    created first app

--- datatools_streamlit.py
+++ datatools_streamlit.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 
 st.title("DataTools Streamlit Demo")
-st.write("Welcome to your very first Streamlit app in Snowflake style!")
+st.write("Welcome to your very first datatoolspro app in Snowflake style!")
