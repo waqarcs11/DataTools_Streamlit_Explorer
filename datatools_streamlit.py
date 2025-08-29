4d8bf91ccd632168477a31e02f581fc182403881
waqarcs11 <adsknock@gmail.com>
2025-08-29 13:23:33 +0500

    jira 1193

--- datatools_streamlit.py
+++ datatools_streamlit.py
@@ -85,7 +85,7 @@ def is_date_type(dtype: str) -> bool:
 # Sidebar: source selection
 # -----------------------------
 with st.sidebar:
-    st.header("1) Select dataset")
+    st.header("1) Choose dataset")
     dbs = list_databases()
     if not dbs:
         st.error("No databases visible for this role.")
